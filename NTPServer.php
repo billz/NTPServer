4c8ff59a99d5a59f505cf94391af09eab5b3c5ce
billz <billzimmerman@gmail.com>
2025-03-16 05:43:36 -0700

    Compatibility fix for php 7.4

--- NTPServer.php
+++ NTPServer.php
@@ -57,7 +57,7 @@ class NTPServer implements PluginInterface
     public function handlePageAction(string $page): bool
     {
         // Verify that this plugin should handle the page
-        if (str_starts_with($page, "/plugin__" . $this->getName())) {
+        if (strpos($page, "/plugin__" . $this->getName()) === 0) {
 
             // Instantiate a StatusMessage object
             $status = new \RaspAP\Messages\StatusMessage;
